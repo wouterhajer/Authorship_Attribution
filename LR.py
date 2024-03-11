22936c932e297caaac768e4a3f7aa834407f3ca5
wouterhajer <19wouter05@gmail.com>
2024-03-11 12:55:52 +0100

    Fixed cllrs cross validation

--- LR.py
+++ LR.py
@@ -32,20 +32,23 @@ if bool(config['masking']['masking']):
     config['masking']['nBestFactorWord'] = 1
 
     if vocab_word == 0:
+        print('hello')
         vocab_word = extend_vocabulary([1, 1], train_df['text'], model='word')
     vocab_word = vocab_word[:n_masking]
+    print(vocab_word)
     vocab_word = [x.lower() for x in vocab_word]
     train_df = mask(train_df, vocab_word, config)
     test_df = mask(test_df, vocab_word, config)
+    full_df = mask(full_df,vocab_word,config)
 
 # Encode author labels
 label_encoder = LabelEncoder()
 train_df['author'] = label_encoder.fit_transform(train_df['author'])
 test_df['author'] = label_encoder.transform(test_df['author'])
+full_df['author'] = label_encoder.transform(full_df['author'])
+#full_df = pd.concat([train_df,test_df])
 
-df = pd.concat([train_df,test_df])
-
-
+df = full_df.copy()
 a = df['conversation'].unique()
 combinations = []
 for comb in itertools.combinations(a, 7):
@@ -68,120 +71,127 @@ use_LSA = bool(config['variables']['useLSA'])
 
 lr = []
 true = []
-"""
-#for i, comb in enumerate(combinations):
-    #print(i)
-    
+#combinations = [([1,2,4,5,6,7,8],[3])]
+for i, comb in enumerate(combinations):
+    print(i)
     del train_df
     del test_df
+    df = full_df.copy()
+
     # Use random or deterministic split
     if bool(config['randomConversations']):
         train_df, test_df = train_test_split(df, test_size=0.125, stratify=df[['author']])
     else:
         train_df, test_df = split(df, 0.125, comb, confusion=bool(config['confusion']))
-"""
-print(train_df)
-print(test_df)
-vocab_char = extend_vocabulary(char_range, train_df['text'], model='char-std')
 
-## initialize tf-idf vectorizer for word n-gram model (captures content) ##
-vectorizer_char = TfidfVectorizer(analyzer='char', ngram_range=char_range, use_idf=True,
-                                  norm='l2', lowercase=lower, vocabulary=vocab_char,
-                                  smooth_idf=True, sublinear_tf=True)
+    train_df = train_df.reset_index(drop=True)
+    test_df = test_df.reset_index(drop=True)
+    #print(train_df,test_df)
 
-train_data_word = vectorizer_char.fit_transform(train_df['text']).toarray()
-print(len(train_data_word))
-n_best = int(len(vectorizer_char.idf_) * n_best_factor)
+    vocab_char = extend_vocabulary(char_range, train_df['text'], model='char-std')
 
-idx_w = np.argsort(vectorizer_char.idf_)[:n_best]
+    ## initialize tf-idf vectorizer for word n-gram model (captures content) ##
+    vectorizer_char = TfidfVectorizer(analyzer='char', ngram_range=char_range, use_idf=True,
+                                      norm='l2', lowercase=lower, vocabulary=vocab_char,
+                                      smooth_idf=True, sublinear_tf=True)
 
-train_data_word = train_data_word[:, idx_w]
-test_data_word = vectorizer_char.transform(test_df['text']).toarray()
-test_data_word = test_data_word[:, idx_w]
+    train_data_word = vectorizer_char.fit_transform(train_df['text']).toarray()
 
-# Choose scaler
-max_abs_scaler = preprocessing.MaxAbsScaler()
-# max_abs_scaler = preprocessing.MinMaxScaler()
+    n_best = int(len(vectorizer_char.idf_) * n_best_factor)
 
-## scale text data for char n-gram model ##
-scaled_train_data_word = max_abs_scaler.fit_transform(train_data_word)
-scaled_test_data_word = max_abs_scaler.transform(test_data_word)
+    idx_w = np.argsort(vectorizer_char.idf_)[:n_best]
 
-num_texts = np.zeros(len(train_data_word[0]))
-for i in range(len(train_data_word)):
-    num_texts += np.array([1 if element > 0 else 0 for element in scaled_train_data_word[i]])
+    train_data_word = train_data_word[:, idx_w]
+    test_data_word = vectorizer_char.transform(test_df['text']).toarray()
+    test_data_word = test_data_word[:, idx_w]
 
-if use_LSA:
-    # initialize truncated singular value decomposition
-    svd = TruncatedSVD(n_components=63, algorithm='randomized', random_state=43)
+    # Choose scaler
+    max_abs_scaler = preprocessing.MaxAbsScaler()
+    # max_abs_scaler = preprocessing.MinMaxScaler()
 
-    # Word
-    scaled_train_data_word = svd.fit_transform(scaled_train_data_word)
-    scaled_test_data_word = svd.transform(scaled_test_data_word)
+    ## scale text data for char n-gram model ##
+    scaled_train_data_word = max_abs_scaler.fit_transform(train_data_word)
+    scaled_test_data_word = max_abs_scaler.transform(test_data_word)
 
+    num_texts = np.zeros(len(train_data_word[0]))
+    for i in range(len(train_data_word)):
+        num_texts += np.array([1 if element > 0 else 0 for element in scaled_train_data_word[i]])
 
+    if use_LSA:
+        # initialize truncated singular value decomposition
+        svd = TruncatedSVD(n_components=63, algorithm='randomized', random_state=43)
 
-suspect = 20
+        # Word
+        scaled_train_data_word = svd.fit_transform(scaled_train_data_word)
+        scaled_test_data_word = svd.transform(scaled_test_data_word)
 
-conversations = list(set(train_df['conversation']))
-for suspect in range(0, len(set(train_df['author']))):
-    train_df['h1'] = [1 if author == suspect else 0 for author in train_df['author']]
+    conversations = list(set(train_df['conversation']))
+    lr_split = []
+    true_split = []
+    for suspect in range(0, len(set(train_df['author']))):
+        train_df['h1'] = [1 if author == suspect else 0 for author in train_df['author']]
 
-    h1_cali = []
-    h2_cali = []
+        h1_cali = []
+        h2_cali = []
 
-    for c in conversations:
+        for c in conversations:
+            char = CalibratedClassifierCV(OneVsRestClassifier(SVC(C=1, kernel='linear',
+                                                                  gamma='auto')))
+            train = train_df.index[train_df['conversation'] != c].tolist()
+
+            calibrate = train_df.index[train_df['conversation'] == c].tolist()
+
+            char.fit(scaled_train_data_word[train], train_df['h1'][train])
+
+            probas_char = char.predict_proba(scaled_train_data_word[calibrate])
+
+            h1_cali.extend([probas_char[i, 0] for i in range(len(probas_char)) if list(train_df['h1'][calibrate])[i] == 1])
+            h2_cali.extend([probas_char[i, 0] for i in range(len(probas_char)) if list(train_df['h1'][calibrate])[i] == 0])
+
         char = CalibratedClassifierCV(OneVsRestClassifier(SVC(C=1, kernel='linear',
                                                               gamma='auto')))
-        train = train_df.index[train_df['conversation'] != c].tolist()
 
-        calibrate = train_df.index[train_df['conversation'] == c].tolist()
-        #print(scaled_train_data_word[train])
-        #print(train_df['h1'][train])
-        #print(len(scaled_train_data_word[train]))
-        #print(len(train_df['h1'][train]))
+        char.fit(scaled_train_data_word, train_df['h1'])
+        probas_val = char.predict_proba(scaled_test_data_word)
 
-        char.fit(scaled_train_data_word[train], train_df['h1'][train])
+        scorer = CalibratedClassifierCV(OneVsRestClassifier(SVC(C=1, kernel='linear',
+                                                                gamma='auto')))
+        calibrator = lir.KDECalibrator(bandwidth='silverman')
+        scorer.fit(scaled_train_data_word, train_df['h1'])
+        dissimilarity_scores_test = scorer.predict_proba(scaled_test_data_word)
 
-        probas_char = char.predict_proba(scaled_train_data_word[calibrate])
+        dissimilarity_scores_train = np.array(h1_cali + h2_cali)
 
-        h1_cali.extend([probas_char[i, 0] for i in range(len(probas_char)) if list(train_df['h1'][calibrate])[i] == 1])
-        h2_cali.extend([probas_char[i, 0] for i in range(len(probas_char)) if list(train_df['h1'][calibrate])[i] == 0])
-    #print(h1_cali)
-    #print(h2_cali)
-
-    char = CalibratedClassifierCV(OneVsRestClassifier(SVC(C=1, kernel='linear',
-                                                          gamma='auto')))
-
-    char.fit(scaled_train_data_word, train_df['h1'])
-    probas_val = char.predict_proba(scaled_test_data_word)
-    #print(test_df.index[test_df.author == suspect])
-    #print(probas_val[:, 0])
-
-    scorer = CalibratedClassifierCV(OneVsRestClassifier(SVC(C=1, kernel='linear',
-                                                            gamma='auto')))
-    calibrator = lir.KDECalibrator(bandwidth='silverman')
-    scorer.fit(scaled_train_data_word, train_df['h1'])
-    dissimilarity_scores_test = scorer.predict_proba(scaled_test_data_word)
-
-    dissimilarity_scores_train = np.array(h1_cali + h2_cali)
-    #print(dissimilarity_scores_train)
-    hypothesis_train = np.array(['H1'] * len(h1_cali) + ['H2'] * len(h2_cali))
-    #print(hypothesis_train)
-    #print(hypothesis_train == 'H1')
-    calibrator.fit(dissimilarity_scores_train, hypothesis_train == 'H1')
+        hypothesis_train = np.array(['H1'] * len(h1_cali) + ['H2'] * len(h2_cali))
+        calibrator.fit(dissimilarity_scores_train, hypothesis_train == 'H1')
 
 
-    bounded_calibrator = lir.ELUBbounder(calibrator)
+        bounded_calibrator = lir.ELUBbounder(calibrator)
 
-    lrs_train = bounded_calibrator.fit_transform(dissimilarity_scores_train, hypothesis_train == 'H1')
-    lrs_test = bounded_calibrator.transform(dissimilarity_scores_test[:,0])
-    #print(lrs_test)
-    #print([1 if author == suspect else 0 for author in test_df['author']])
-    lr.extend(lrs_test)
-    true.extend([1 if author == suspect else 0 for author in test_df['author']])
-    time.sleep(0.1)
-    df = df.sort_index(axis=0)
+        lrs_train = bounded_calibrator.fit_transform(dissimilarity_scores_train, hypothesis_train == 'H1')
+        lrs_test = bounded_calibrator.transform(dissimilarity_scores_test[:,0])
+
+        lr.extend(lrs_test)
+        true.extend([1 if author == suspect else 0 for author in test_df['author']])
+        lr_split.extend(lrs_test)
+        true_split.extend([1 if author == suspect else 0 for author in test_df['author']])
+        time.sleep(0.1)
+    index = [i for i in range(len(true_split)) if true_split[i] == 1]
+    print(np.array(lr_split)[index])
+    cllr = lir.metrics.cllr(np.array(lr_split), np.array(true_split))
+    cllr_min = lir.metrics.cllr_min(np.array(lr_split), np.array(true_split))
+    cllr_cal = cllr - cllr_min
+    print(f"Test conversation: {comb[1]}")
+    print(f"Cllr: {cllr}, Cllr_min: {cllr_min}, Cllr_cal: {cllr_cal}")
+    with lir.plotting.show() as ax:
+        ax.calibrator_fit(calibrator, score_range=[0, 1])
+        ax.score_distribution(scores=dissimilarity_scores_train, y=(hypothesis_train == 'H1') * 1,
+                              bins=np.linspace(0, 1, 20), weighted=True)
+        ax.xlabel('SVM score')
+        H1_legend = mpatches.Patch(color='tab:blue', alpha=.3, label='$H_1$-true')
+        H1_legend = mpatches.Patch(color='tab:orange', alpha=.3, label='$H_1$-true')
+
+    plt.show()
 
 with lir.plotting.show() as ax:
     ax.calibrator_fit(calibrator, score_range=[0, 1])
@@ -195,13 +205,13 @@ plt.show()
 plt.scatter(dissimilarity_scores_train, np.log10(lrs_train))
 plt.show()
 
-plt.scatter(true,np.log10(lr))
+plt.scatter(true, np.log10(lr))
 plt.show()
 df = df.sort_index(axis=0)
-print(df)
-index=[i for i in range(len(true)) if true[i] == 1]
+print(train_df)
+index = [i for i in range(len(true)) if true[i] == 1]
 print(np.array(lr)[index])
-cllr = lir.metrics.cllr(np.array(lr),np.array(true))
-cllr_min = lir.metrics.cllr_min(np.array(lr),np.array(true))
+cllr = lir.metrics.cllr(np.array(lr), np.array(true))
+cllr_min = lir.metrics.cllr_min(np.array(lr), np.array(true))
 cllr_cal = cllr-cllr_min
 print(f"Cllr: {cllr}, Cllr_min: {cllr_min}, Cllr_cal: {cllr_cal}")
